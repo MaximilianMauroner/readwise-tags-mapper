5c70c398a341be3fd886631c23eed2146804124c
Maximilian Mauroner <github@relay.mauroner.eu>
2025-10-06 19:45:30 +0200

    est

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,3 @@
-export const config = {
-  runtime: "bun@1.1.20",
-};
-
-export default function handler(_req: Request): Response {
-  return new Response("hello", {
-    status: 200,
-    headers: { "content-type": "text/plain; charset=utf-8" },
-  });
+export function GET(request: Request) {
+  return new Response("Hello from Vercel!");
 }
